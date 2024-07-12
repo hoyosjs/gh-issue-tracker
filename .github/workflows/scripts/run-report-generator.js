f0f1ce67b80ce278341c182b7df11da3ace49b73
Juan Sebastian Hoyos Ayala <juhoyosa@microsoft.com>
2024-07-12 01:16:04 +0000

    more workflow_call woes

--- .github/workflows/scripts/run-report-generator.js
+++ .github/workflows/scripts/run-report-generator.js
@@ -2,23 +2,9 @@
 import { constants } from 'fs';
 import path from 'path';
 import * as promises from 'fs/promises';
+import { DefaultArtifactClient } from '@actions/artifact';
 
-import { DefaultArtifactClient } from '@actions/artifact'
-
-export default async function generateReportsEntrypoint({ core, exec }) {
-  try {
-    await generateReports({ core, exec });
-  } catch (error) {
-    core.error(error);
-    core.setFailed(error);
-  }
-}
-
-async function generateReports({ core, exec }) {
-  core.startGroup('Get Configurations');
-  const configs = await getConfigurations(core);
-  core.endGroup();
-
+export default async function generateReportsEntrypoint({ inputs, core, exec }) {
   const execOptions = {
     listeners: {
       stdout: (data) => {
@@ -31,28 +17,114 @@ async function generateReports({ core, exec }) {
     failOnStdErr: true
   };
 
-  core.startGroup(`Run generate-all-reports for ${configs.configFilePath}`);
-  await generateIssueReports(configs, execOptions);
-  core.endGroup();
+  try {
+    core.startGroup('Get Configurations');
+    const configs = await getConfigurations({core, inputs});
+    core.endGroup();
 
-  core.startGroup('Upload Reports');
-  const folderDate = await findLatestCacheDate(configs.reportDirectory);
-  const filesGenerated = getCacheFilesPaths(configs, folderDate);
-  if (configs.shouldUpdateCaches) {
-    await uploadResultsToCache(filesGenerated);
-  } else {
-    await uploadResultsAsArtifact(filesGenerated);
+    core.startGroup(`Run generate-all-reports for ${configs.configFilePath}`);
+    await generateReports({ core, exec, execOptions, configs});
+    core.endGroup();
+
+    core.startGroup('Update caches and reports');
+    await updateAndUploadReports({ core, exec, execOptions, configs });
+    core.endGroup();
+
+    core.info('Report generation completed successfully!');
+
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error);
   }
-  core.endGroup();
+}
 
-  core.info('Report generation completed successfully!');
+async function getConfigurations({core, inputs}) {
+  let loadedConfig = {
+    cacheFile: ''
+  };
 
-  async function uploadResultsAsArtifact(filesGenerated) {
+  // Nasty... using inputs instead of core.getInput. But this doesn't play well in workflow_call
+  loadedConfig.shouldUpdateCaches = 'shouldUpdateCaches' in inputs && inputs.shouldUpdateCaches === 'true';
+
+  // Try to find the specified config file
+  if (!('configName' in inputs)) {
+    throw new Error("No config file specified");
+  }
+
+  try {
+    const configFilePath = path.join('Configs', `${inputs.configName}.json`)
+    loadedConfig.configFilePath = path.resolve(configFilePath);
+    const configContents = await promises.readFile(loadedConfig.configFilePath, 'utf8');
+    const config = JSON.parse(configContents);
+
+    loadedConfig.reportDirectory = config.ReportConfig.ReportOutputPath;
+    loadedConfig.reportFriendlyName = config.ReportConfig.ReportFriendlyName;
+    loadedConfig.cacheFilesPrefix = config.ReportConfig.ReportFilePrefix;
+
+    core.info(`Fully loaded config: "${loadedConfig.configFilePath}"`);
+  } catch (error) {
+    core.error(`Unable to parse or find config file ${inputs.configName}`);
+    throw error;
+  }
+
+  await promises.mkdir(loadedConfig.reportDirectory, { recursive: true })
+
+  // Try to find the specified cache file or load the latest one.
+  try {
+    var cache_date = '';
+
+    if ('oldConfigDate' in inputs) {
+      cache_date = inputs.oldConfigDate;
+    } else {
+      core.info("No cache date specified. Finding latest cache date.");
+      cache_date = await findLatestCacheDate(loadedConfig.reportDirectory);
+    }
+
+    if (cache_date === '') {
+      core.info("No cache data found.");
+      loadedConfig.cacheFile = '';
+    } else {
+      const cached_list = path.join(loadedConfig.reportDirectory, cache_date, `${loadedConfig.cacheFilesPrefix}-cache.json`);
+      await promises.access(cached_list, constants.R_OK).catch(() => {
+        throw new Error(`Cached result not found at ${cached_list}.`)
+      });
+      core.info(`Using cached result: "${cached_list}"`);
+      loadedConfig.cacheFile = cached_list;
+    }
+  } catch (error) {
+    core.error("Unable to find cache file");
+    throw error;
+  }
+
+  return loadedConfig;
+}
+
+async function generateReports({ exec, execOptions, configs }) {
+  await exec.exec("dotnet", ["run",
+    "--project", "gh-issue-tracker.csproj",
+    "-c", "release",
+    "--",
+    "generate-all-reports", configs.configFilePath, configs.cacheFile],
+    execOptions);
+}
+
+async function updateAndUploadReports({ core, exec, execOptions, configs }) {
+
+  const folderDate = await findLatestCacheDate(configs.reportDirectory);
+  const filesGenerated = getLatestGeneratedFiles(configs, folderDate);
+
+  if (configs.shouldUpdateCaches) {
+    await uploadResultsToCache(exec, configs, filesGenerated);
+  } else {
+    await uploadResultsAsArtifact(configs, filesGenerated);
+  }
+
+  async function uploadResultsAsArtifact(configs, filesGenerated) {
     const artifact = new DefaultArtifactClient();
     await artifact.uploadArtifact('report', Object.values(filesGenerated), configs.reportDirectory, { retentionDays: 5 });
   }
 
-  async function uploadResultsToCache(filesGenerated) {
+  async function uploadResultsToCache(exec, configs, filesGenerated) {
     let targetFullReport = path.resolve(path.join(configs.reportDirectory, `${configs.cacheFilesPrefix}-full.md`));
     promises.rename(filesGenerated.fullReport, targetFullReport);
     core.info(`Full report moved to ${targetFullReport}`);
@@ -73,69 +145,16 @@ async function generateReports({ core, exec }) {
     await exec.exec('git', ['push'], execOptions);
   }
 
-  async function generateIssueReports(configs) {
-    await exec.exec("dotnet", ["run",
-      "--project", "gh-issue-tracker.csproj",
-      "-c", "release",
-      "--",
-      "generate-all-reports", configs.configFilePath, configs.cacheFile],
-      execOptions);
-  }
-}
+  function getLatestGeneratedFiles(config, folderDate) {
+    const baseDir = path.resolve(config.reportDirectory);
 
-async function getConfigurations(core) {
-  let loadedConfig = {
-    cacheFile: ''
-  };
-
-  var shouldUpdateCaches = core.getInput('shouldUpdateCaches', { required: true });
-  loadedConfig.shouldUpdateCaches = shouldUpdateCaches === 'true';
-
-  // Try to find the specified config file
-  try {
-    const configFileName = core.getInput('configName', { required: true });
-    var configFilePath = path.join('Configs', `${configFileName}.json`)
-    loadedConfig.configFilePath = path.resolve(configFilePath);
-
-    const configContents = await promises.readFile(loadedConfig.configFilePath, 'utf8');
-    const config = JSON.parse(configContents);
-    loadedConfig.reportDirectory = config.ReportConfig.ReportOutputPath;
-    loadedConfig.reportFriendlyName = config.ReportConfig.ReportFriendlyName;
-    loadedConfig.cacheFilesPrefix = config.ReportConfig.ReportFilePrefix;
-    core.info(`Fully loaded config: "${loadedConfig.configFilePath}"`);
-  } catch (error) {
-    core.error("Unable to parse config file");
-    throw error;
-  }
-
-  await promises.mkdir(loadedConfig.reportDirectory, { recursive: true })
-
-  // Try to find the specified cache file or load the latest one.
-  try {
-    var cache_date = core.getInput('oldConfigDate', { required: false });
-
-    if (!cache_date) {
-      core.info("No cache date specified. Finding latest cache date.");
-      cache_date = await findLatestCacheDate(loadedConfig.reportDirectory);
+    return {
+      cacheFile: path.join(baseDir, folderDate, `${config.cacheFilesPrefix}-cache.json`),
+      compReport: path.join(baseDir, folderDate, `${config.cacheFilesPrefix}-comp.md`),
+      csvTotals: path.join(baseDir, `${config.cacheFilesPrefix}-totals.csv`),
+      fullReport: path.join(baseDir, folderDate, `${config.cacheFilesPrefix}-full.md`),
     }
-
-    const cached_list = cache_date === ''
-      ? ''
-      : path.join(loadedConfig.reportDirectory, cache_date, `${loadedConfig.cacheFilesPrefix}-cache.json`);
-
-    if (cached_list !== '') {
-      await promises.access(cached_list, constants.R_OK).catch(() => {
-        throw new Error(`Cached result not found at ${cached_list}.`)
-      });
-      core.info(`Using cached result: "${cached_list}"`);
-      loadedConfig.cacheFile = cached_list;
-    }
-  } catch (error) {
-    core.error("Unable to find cache file");
-    throw error;
   }
-
-  return loadedConfig;
 }
 
 async function findLatestCacheDate(reportDirectory) {
@@ -143,17 +162,6 @@ async function findLatestCacheDate(reportDirectory) {
   var date_list = await promises.readdir(reportDirectory, { withFileTypes: true });
   date_list = date_list.filter(dirent => dirent.isDirectory()).sort((a, b) => (a.name > b.name ? -1 : -1));
   return date_list.length > 0 ? date_list[0].name : '';
-}
-
-function getCacheFilesPaths(config, folderDate) {
-  const baseDir = path.resolve(config.reportDirectory);
-
-  return {
-    cacheFile: path.join(baseDir, folderDate, `${config.cacheFilesPrefix}-cache.json`),
-    compReport: path.join(baseDir, folderDate, `${config.cacheFilesPrefix}-comp.md`),
-    csvTotals: path.join(baseDir, `${config.cacheFilesPrefix}-totals.csv`),
-    fullReport: path.join(baseDir, folderDate, `${config.cacheFilesPrefix}-full.md`),
-  }
 }
 
 // Purely for test when  running locally.
